e263c5d5f166ba9ac16153f356d5cfc4263b2172
pengkuh262 <hidayatkukuh75@gmail.com>
2022-11-09 12:35:17 +0700

    Commit pertama

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Mulish } from "@next/font/google";
+
+const mulish = Mulish({
+  weight: "400",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={mulish.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
