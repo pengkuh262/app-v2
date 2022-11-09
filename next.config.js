22a9c7e22ca772c4f214dbeca67afcaecceeccd9
pengkuh262 <hidayatkukuh75@gmail.com>
2022-11-09 12:57:08 +0700

    Commit pertama

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
